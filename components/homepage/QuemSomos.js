04ce70714e1740c46986a1e1a08fe6a1b06b53f3
zebedal <zebedal@bitbucket.org>
2021-10-05 22:01:42 +0100

    flex fix

--- components/homepage/QuemSomos.js
+++ components/homepage/QuemSomos.js
@@ -3,22 +3,24 @@ import Image from 'next/image'
 import Button from '../UI/Button'
 
 const QuemSomos = props => (
-    <div className={`container ${styles.flex}`}>
-        <div className={styles.imgWrapper}>
-            <Image src="/assets/img/nos.jpg" layout="fill" alt="psicologia e psicoterapia"/>
-        </div>
-        <div className={styles.content}>
-            <strong>Quem somos</strong>
-            <h2>Somos o seu melhor <span>suporte</span></h2>
-            <br />
-            <br />
-            <br />
-            <p>
-                Et nostrud proident proident minim in qui qui qui nulla sint ad ad exercitation tempor. Consectetur tempor laborum ipsum ullamco sint in do irure in. Sunt officia cupidatat pariatur qui esse duis excepteur adipisicing ullamco. Exercitation nisi ex nostrud aliqua et.
-            </p>
-            <br/>
-            <br/>
-            <Button text="Sobre nós" route="/sobre"/>
+    <div className="container">
+        <div className={` ${styles.flex}`}>
+            <div className={styles.imgWrapper}>
+                <Image src="/assets/img/nos.jpg" layout="fill" alt="psicologia e psicoterapia" />
+            </div>
+            <div className={styles.content}>
+                <strong>Quem somos</strong>
+                <h2>Somos o seu melhor <span>suporte</span></h2>
+                <br />
+                <br />
+                <br />
+                <p>
+                    Et nostrud proident proident minim in qui qui qui nulla sint ad ad exercitation tempor. Consectetur tempor laborum ipsum ullamco sint in do irure in. Sunt officia cupidatat pariatur qui esse duis excepteur adipisicing ullamco. Exercitation nisi ex nostrud aliqua et.
+                </p>
+                <br />
+                <br />
+                <Button text="Sobre nós" route="/sobre" />
+            </div>
         </div>
     </div>
 )
